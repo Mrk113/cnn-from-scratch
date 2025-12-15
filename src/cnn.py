aff0642cd8a4603cbe800a0346626eefe136a8f9
Mark Karpov <mark.karpov@gmx.de>
2025-12-15 20:55:38 +0100

    Basic cnn training setup

--- src/cnn.py
+++ src/cnn.py
@@ -1,1 +1,29 @@
-# TODO: Implement CNN model
+from core.utils.train import train
+from core.losses.mse import MSE
+
+
+X, Y = load()  
+
+# miniVGG
+network = [
+    Conv,
+    ReLu,
+    Conv,
+    ReLu,
+
+    Pool,
+
+    Conv,
+    ReLu,
+    Conv,
+    ReLu,
+
+    Pool,
+
+    FC,
+    ReLu,
+    FC,
+    ReLu,
+]
+
+train(network, MSE, X, Y, epochs=250, learning_rate=0.01, batch_size=32, verbose=True)
