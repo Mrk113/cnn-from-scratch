2bccf9f81cd7d1421e92be10ebedab675b7ddfe0
Mrk113 <mark.karpov@gmx.de>
2026-01-05 21:22:01 +0100

    import update

---
+++ src/core/__init__.py
@@ -0,0 +1,15 @@
+from core.layers.activation.relu import ReLU
+from core.layers.activation.softmax import Softmax 
+
+from core.layers.compute.conv import Conv
+from core.layers.compute.fc import FC
+from core.layers.compute.flatten import Flatten
+
+from core.layers.pooling.avg_pool import AvgPool
+from core.layers.pooling.max_pool import MaxPool
+
+from core.losses.mse import MSE
+from core.losses.cross_entropy import CrossEntropy
+
+from core.utils.train import Data, TrainConfig, train
+from core.utils.load import load
