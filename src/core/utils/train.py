e957a854cf4b4fc7e2501d32bb205d6eed6f79a1
Mrk113 <mark.karpov@gmx.de>
2026-01-05 21:20:22 +0100

    Implement Utils

--- src/core/utils/train.py
+++ src/core/utils/train.py
@@ -1,14 +1,53 @@
-def train(network, loss, X, Y, epochs=300, learning_rate=0.01, batch_size=32, verbose=True, log=False):
-    for epoch in range(epochs):
+import cupy as cp
+from dataclasses import dataclass
+
+
+@dataclass
+class Data:
+    X: cp.ndarray
+    Y: cp.ndarray
+
+@dataclass
+class TrainConfig:
+    epochs: int
+    learning_rate: float
+    batch_size: int
+    verbose: bool
+    eval_every: int
+
+
+def forward(network, X):
+    output = X
+    for layer in network:
+        output = layer.forward(output)
+    return output
+
+
+def evaluate(network, loss, data: Data, batch_size: int):
+    return 1, 1
+
+
+def train(
+    network,
+    loss,
+    train_data: Data,
+    val_data: Data,
+    config: TrainConfig,
+):
+    history = {
+        "train_loss": [],
+        "val_loss": [],
+        "val_acc": [],
+    }
+
+    for epoch in range(config.epochs):
         error = 0
-        for i in range(0, X.shape[0], batch_size):
-            X_batch = X[i:i+batch_size]
-            Y_batch = Y[i:i+batch_size]
+        for i in range(0, train_data.X.shape[0], config.batch_size):
+            X_batch = train_data.X[i : i + config.batch_size]
+            Y_batch = train_data.Y[i : i + config.batch_size]
 
             # Forward pass
-            output = X_batch
-            for layer in network:
-                output = layer.forward(output)
+            output = forward(network, X_batch)
 
             # Compute loss
             batch_error = loss.compute(output, Y_batch)
@@ -17,10 +56,20 @@ def train(network, loss, X, Y, epochs=300, learning_rate=0.01, batch_size=32, ve
             # Backward pass
             grad = loss.gradient(output, Y_batch)
             for layer in reversed(network):
-                grad = layer.backward(grad, learning_rate)
+                grad = layer.backward(grad, config.learning_rate)
 
-        num_batches = (X.shape[0] + batch_size - 1) // batch_size
-        error /= num_batches
+        num_batches = (train_data.X.shape[0] + config.batch_size - 1) // config.batch_size
+        error /= max(1, num_batches)
 
-        if verbose:
-            print(f"Epoch: {epoch+1}/{epochs} - Loss: {error}")
+        train_loss = float(error.get())
+        history["train_loss"].append(train_loss)
+
+            
+        val_loss, val_acc = evaluate(network, loss, val_data, batch_size=config.batch_size)
+        history["val_loss"].append(val_loss)
+        history["val_acc"].append(val_acc)
+
+        if config.verbose:
+            print(f"Epoch: {epoch+1}/{config.epochs} - Train Loss: {train_loss:.6f}")
+
+    return history
